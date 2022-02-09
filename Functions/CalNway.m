0396ac796133de145d23297a853d5787d5517124
yzw <625715875@qq.com>
2022-02-09 21:37:24 +0800

    add the overFlag and patch_size params.

--- Functions/CalNway.m
+++ Functions/CalNway.m
@@ -1,4 +1,4 @@
-function [Nway, x_size, y_size, x_start, y_start] = CalNway(imgsize)
+function [Nway, x_size, y_size, x_start, y_start] = CalNway(imgsize,overFlag,mini_patch_size)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CalNway: 
 %   - Calculate the Nway parameter (Algorithm 2).
@@ -15,22 +15,30 @@ ch_size = imgsize(3);
 
 num4 = 1; 
 
-imgrow = CalSize(imgrow);
+if nargin == 1
+    overFlag = 1;  % default is 1, namely, with overlapping
+end
+
+if nargin < 3
+    mini_patch_size = 4;  % default is 4, namely the most augmented
+end
+
+imgrow = CalSize(imgrow,overFlag);
 x_size(num4) = imgrow;
 x_start(num4) = imgsize(1) - x_size(num4) + 1;
 
-imgcol = CalSize(imgcol);
+imgcol = CalSize(imgcol,overFlag);
 y_size(num4) = imgcol;
 y_start(num4) = imgsize(2) - y_size(num4) + 1;
 
-while imgrow > 4 && imgcol > 4
+while imgrow > mini_patch_size && imgcol > mini_patch_size
     num4 = num4+1; 
     
-    imgrow = CalSize(imgrow);
+    imgrow = CalSize(imgrow,overFlag);
     x_size(num4) = imgrow;
     x_start(num4) = x_size(num4-1) - x_size(num4) + 1;
     
-    imgcol = CalSize(imgcol);  
+    imgcol = CalSize(imgcol,overFlag);  
     y_size(num4) = imgcol;
     y_start(num4) = y_size(num4-1) - y_size(num4) + 1;
 end
--- Functions/CalSize.m
+++ Functions/CalSize.m
@@ -1,4 +1,4 @@
-function [sub_size] = CalSize (msize)
+function [sub_size] = CalSize (msize,overFlag)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CalSize: 
 %   - Calculate the sub-block size (Eq. (17) in the paper).
@@ -7,11 +7,24 @@ function [sub_size] = CalSize (msize)
 % Output:  
 %   - The sub-block size after overlapping.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sub_size = (msize+1)/2;
-if msize == 3
-    sub_size= 2;
-elseif sub_size == fix(sub_size)
-    sub_size= sub_size+ 1;
-else  
-    sub_size= ceil(sub_size);
+if nargin == 1
+    overFlag = 1;
 end
+
+if logical(overFlag) == 1   % overlap 2 or 3 element case
+    sub_size = (msize+1)/2;
+    if msize == 3
+        sub_size= 2;
+    elseif sub_size == fix(sub_size)
+        sub_size= sub_size+ 1;
+    else
+        sub_size= ceil(sub_size);
+    end
+    
+elseif logical(overFlag) == 0  % no overlap case
+    sub_size = (msize)/2;
+end
+
+end    
+    
+   
